035728b912b9a37db28b5c508340a0c1c276b837
Thor Andresen <thorg3@gmail.com>
2021-10-05 09:13:23 +0200

    tried to add scss

---
+++ website_vue_andreas/src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<script src="../js/_mainpage.js"></script>
+
+<template class="template-main">
+  <div class="hello">
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+@import "../css/_mainpage.css";
+</style>
